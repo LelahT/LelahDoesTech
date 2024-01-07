a9fbd1c9dedcb254e49b976a671bdc2ce037b3e2
Lelah Tehmeh <139195548+LelahT@users.noreply.github.com>
2024-01-06 21:52:41 -0500

    Add files via upload
    Hello World! This was a project that was 10 hours in the making. It was a coding challenge given by a company that is interested in hiring me. I learned so much during this time. I was frustrated, did more tests than I can count on my hand but it was all worth it watching this little guy move. I hope I get the job, but if not I still learned a few new things and had the most fun along the way. console.log("Here We Go Again");

---
+++ solution.ts
@@ -0,0 +1,103 @@
+class Stacker {
+
+  currentPosition: {x : number, y: number };
+
+  hasBlock: boolean;
+
+  hasTreasure: boolean;
+
+  hasWall: boolean;
+
+  constructor() {
+    this.currentPosition = { x: 0, y:0};
+    this.hasTreasure = false;
+    this.hasBlock = false;
+    this.hasWall = false;
+  }
+
+turn = (mapInfo:{
+ 'left': {
+   type: number;
+   level: number;
+ };
+ 'up': {
+   type: number;
+   level: number;
+ };
+ 'right': {
+   type: number;
+   level: number;
+ };
+ 'down': {
+   type: number;
+   level: number;
+ };
+ 'type': number;
+ 'level': number;
+
+}): string => {
+  
+  // Pick an action randomly
+	var n = Math.random() * 6 >> 0;
+	if (n == 0) return "left";
+	if (n == 1) return "up";
+	if (n == 2) return "right";
+	if (n == 3) return "down";
+	if (n == 4) return "pickup";
+	if (n == 5) return "drop";
+
+ // Check if there is a wall
+ if (mapInfo.left.type === 1
+      || mapInfo.up.type === 1
+      || mapInfo.right.type === 1
+      || mapInfo.down.type === 1) {
+       this.hasWall = true;
+ }
+
+// Check if there is a block at the current position & Pick Up a block
+  if (mapInfo.type === 2) {
+    this.hasBlock = true;
+    return 'pickup';
+  }
+
+// Check if the treasure is found
+ if (mapInfo.type === 3) {
+  this.hasTreasure = true;
+}
+
+ // Move around a wall if True
+ if (this.hasWall) {
+   if (mapInfo.up.type === 1) {
+     return 'down';
+   } if (mapInfo.down.type === 1) {
+     return 'right';
+   } if (mapInfo.right.type === 1) {
+     return 'left';
+   }
+ }
+
+
+ // If the agent has a block, build a staircase
+ if (this.hasBlock) {
+  return mapInfo.level <= 1 ? 'drop' : 'up';
+ } else {
+
+ // Move towards the treasure if not found
+ if (this.hasTreasure) {
+   if (mapInfo.right.type === 0) {
+     return 'right';
+   } if (mapInfo.up.type === 0) {
+     return 'up';
+   } if (mapInfo.left.type === 0) {
+     return 'left';
+   } if (mapInfo.down.type === 0) {
+     return 'down';
+   }
+ }
+}
+
+ // Default action is to wait
+ return '';
+}
+}
+const _default = Stacker;
